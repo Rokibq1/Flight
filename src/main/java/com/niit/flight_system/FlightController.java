78fcaa526a72f440298b675d91cf9e886fc6f3de
Rokibq1 <rokibmusa13@gmail.com>
2026-05-11 14:20:25 +0100

    Initial Commit

---
+++ src/main/java/com/niit/flight_system/FlightController.java
@@ -0,0 +1,28 @@
+package  com.niit.flight-system;
+
+
+import   org.springframework.stereotype.Controller;
+import   org.springframework.ui.Model;
+import   org.springframework.web.bind.annotation.GetMapping;
+import   java.until.Arrays;
+import   java.until.list;
+
+
+@Controller
+
+public  class FlightController{
+
+@GetMapping("/")
+public  String  viewDashboard(Model  model){
+
+
+model.addAttribute("username",  "Instructor  Wisdom");
+
+model.addAttribute("systemStatus",  "Operational");
+
+List<String>  flights  = Arrays.asList("NIIT-101(Lagos)",  "NIIT-202(London)", "NIIT-303(Dubai)");
+model.addAttribute("activeFlights",flights);
+
+return  "index"; 
+}
+}
